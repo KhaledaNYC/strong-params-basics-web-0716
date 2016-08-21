0b92a4fc5c132a808909eff82f62106ddc3396e4
KhaledaNYC <khaleda.khan87@gmail.com>
2016-08-20 22:28:51 -0400

    strong params1

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,14 +16,14 @@ class PostsController < ApplicationController
 	  @post.save
 	  redirect_to post_path(@post)
 	end
-
+	def edit
+	  @post = Post.find(params[:id])
+	end
 	def update
 	  @post = Post.find(params[:id])
 	  @post.update(params["post"])
 	  redirect_to post_path(@post)
 	end
 
-	def edit
-	  @post = Post.find(params[:id])
-	end
+
 end
